dbaca945f993b997e3379c4c5e7ebd58ac778ad7
ascexz <764000395@qq.com>
2017-10-24 21:15:15 +0800

    随机数定时器

--- A_3_2/MainWindow.xaml.cs
+++ A_3_2/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+//引入wpf中用的定时器
+using System.Windows.Threading;
 
 namespace A_3_2
 {
@@ -23,6 +26,43 @@ namespace A_3_2
         public MainWindow()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            start.Click += delegate
+            {
+                if (shi.IsChecked == true)
+                {
+                    result.Content = "随机生成实数功能暂未实现";
+                }
+                else {
+                    try {
+                        int min = int.Parse(minText.Text);
+                        int max = int.Parse(maxText.Text);
+                        //int interval;
+                        if (!int.TryParse(intervalText.Text, out int interval)) {
+                            result.Content = "时间间隔应为正整数，请重新输入。";
+                            return;
+                        }
+                        if ( max < min ) {
+                            result.Content = "最小范围大于最大范围，\n请重新输入生成随机数的范围";
+                            return;
+                        }
+                        
+                        timer.Interval = new TimeSpan(interval);
+                        timer.Start();
+                        timer.Tick += delegate {
+                            result.Content = RandomHelp.getIntRandomNumber(min, max);
+                        };
+
+                        stop.Click += delegate {
+                            timer.Stop();
+                        };
+                    }
+                    catch (Exception e) {
+                        result.Content = e.Source + "\n请重新设置随机数的范围";
+                    }
+                }
+
+            };
         }
     }
 }
---
+++ A_3_2/RandomHelp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_3_2
+{
+    class RandomHelp
+    {
+        public static Random rand = new Random();
+        public static int getIntRandomNumber(int min, int max) {         
+            return rand.Next(min, max);
+        }
+
+        public static double doubleRandomNumber() {
+            rand.Next();
+            return 0;
+        }
+    }
+}
